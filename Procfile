b1dcf7a0612da3a86bd5b334992d0baaf7446778
Brajan <brajan89089@gmail.com>
2026-03-17 13:45:17 +0530

    Configure project for Railway deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bikehub_proj.wsgi:application
--- bikehub_proj/settings.py
+++ bikehub_proj/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.ngrok-free.dev',
+    'https://*.up.railway.app',
 ]
 
 CSRF_COOKIE_SECURE = False
@@ -60,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_URL = '/login/'
