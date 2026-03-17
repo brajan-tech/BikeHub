db6414100b529c5ef74b726e96882ce0f4d69527
Brajan <brajan89089@gmail.com>
2026-03-17 21:21:20 +0530

    fix email error

--- bikehub_proj/settings.py
+++ bikehub_proj/settings.py
@@ -148,7 +148,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
